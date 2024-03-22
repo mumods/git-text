af61c453ecc928c2493dbd9eacdb96c00c540fb9
MaleX <89131321+mumods@users.noreply.github.com>
2024-03-22 18:25:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <div>git测试1</div>
+    <div>git测试0</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    zzzzzz
   </nav>
   <router-view/>
 </template>
