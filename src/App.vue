5e05648473f394aa505c9fb5d8f788704824576f
momuds <1076278060@qq.com>
2024-03-22 16:45:23 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <nav>
-    <div>git测试3</div>
+<<<<<<< Updated upstream
     <div>git测试2</div>
+=======
+    <div>git测试1</div>
+    <div>git测试0</div>
+    <div>111111</div>
+>>>>>>> Stashed changes
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
