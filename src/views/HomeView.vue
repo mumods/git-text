ff8425122c1fbb989a6a5756137252f1b67c0283
unknown <1076278060@qq.com>
2024-03-22 15:25:48 +0800

    a

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img ref = 'aimg' alt="Vue logo" src="../assets/logo.png">
+    <input ref="ing" type="text">
+  </div>
+</template>
+
+<script lang="ts" setup>
+let a=1
+for (let index = 0; index < 5; index++) {
+  a = a+index
+}
+console.log(a);
+
+</script>
