010b2af4ece166ac0aff18cb41b86237f8d2a841
momuds <1076278060@qq.com>
2024-03-22 18:42:23 +0800

    as

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
+    <div>git测试0000</div>
     <div>git测试0</div>
     <div>git测试1</div>
     <div>git测试2</div>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
